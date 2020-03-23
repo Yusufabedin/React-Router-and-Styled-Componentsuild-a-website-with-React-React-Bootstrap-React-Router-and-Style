e58d7a62c74938bba141bf54fb3c0bf67f403cfb
Yusufabedin <yusuf.abedin95@gmail.com>
2020-03-24 00:42:08 +0600

    first commit

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>I am Professional Full Stack Web Developer.</p>
+    <p>
+      Highly experienced in HTML5, CSS3,Bootstrap, JavaScript, jQuery, WordPress
+      and many more...
+    </p>
+    <p>
+      I am always willing to help my customers and offer them the best web
+      project. Send me a message so we can start building your new website!
+    </p>
+  </div>
+);
---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Contact = () => (
+  <div>
+    <h1>Contact Us</h1>
+    <p>
+      !PLEASE CONTACT ME BEFORE Just let me know your requirements and you will
+      get 100% perfect work?
+    </p>
+  </div>
+);
